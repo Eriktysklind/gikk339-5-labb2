21d29a51575909fa44af08b58de8c28c662f3ffd
Eriktysklind <h23eribl@du.se>
2024-12-17 10:14:16 +0100

    commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+  });
+
+server.listen(3000, () => console.log("Kör server 3000"));
+
+console.log("hej");
