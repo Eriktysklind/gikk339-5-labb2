3fad0279f5163f7ede2066aa018ee2879d9df39d
Eriktysklind <h23eribl@du.se>
2024-12-18 10:11:05 +0100

    commit

--- client/script.js
+++ client/script.js
@@ -13,19 +13,20 @@ fetch(url)
     return response.json();
   })
   .then((users) => {
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i];
+    users.forEach((users) => {
       const li = document.createElement("li");
-      li.style.backgroundColor = user.color;
+      //Stylar list elemeten
+      li.style.backgroundColor = users.color;
       li.style.padding = "1rem";
       li.style.margin = "0.5rem";
       li.style.listStyleType = "none";
-      li.innerHTML = `<p>${user.id}.  ${user.firstName} ${user.lastName}</p> <p> ${user.username}</p>  <p>${user.color}</p>`;
+      li.innerHTML = `<h3>${users.id}.  ${users.firstName} ${users.lastName}</h3> <p> ${users.username}</p>  <p>${users.color}</p>`;
 
       ul.appendChild(li);
-    }
+    });
   });
-const newDiv = document.createElement("div");
+
 document.body.appendChild(ul);
+
 /* const newDiv = document.getElementById("newDiv");
 newDiv.appendChild(ul); */
