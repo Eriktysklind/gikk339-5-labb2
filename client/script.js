39b59d610a211d92a35ca5d996905e310a4ef24f
Davidskaring <h23davsk@du.se>
2024-12-18 13:55:43 +0100

    123

--- client/script.js
+++ client/script.js
@@ -1,13 +1,14 @@
 const url = "http://localhost:3000/users";
 const ul = document.createElement("ul");
 
-fetch(url)
+fetch(url) // funktionen fetch() är inbyggt i javascript och används för att skicka och ta emot data över ett nätverk, alltså över http.
   .then((response) => {
+    // använder vi then() för att hantera  det asynkrona anropet response
     console.log(response);
     return response.json();
   })
   .then((jsonData) => console.log(jsonData));
-
+// vi välde att använde then() för att hantera det asynkrona anropet då. det är den metoden vi kände oss mest bekäva med då vi använt den tidigare i lektionerna.
 fetch(url)
   .then((response) => {
     return response.json();
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = express();
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3").verbose(); // importerar modulen för sqlite3 och spara den i variablen sqlite3
+
+// vilka kommandon i termnialen och rader kod möjliggör att ni kan använda sqlite i erat node.js.?  npm install sqlite3
 
 server
   .use(express.json())
@@ -13,12 +15,13 @@ server
   });
 
 server.get("/users", (req, res) => {
-  const db = new sqlite3.Database("./server/gik339-labb2.db");
+  const db = new sqlite3.Database("./server/gik339-labb2.db"); // på  raden där vi skapar variablen db där vi skapar upp en ny databas och skapar en uppkoppling mellan databasen och filen.
   db.all("SELECT * FROM USERS", (err, rows) => {
     if (err) {
+      // if statsen innehåller raderna kod som skicker respons till klienten beroende på om det uppstod
       res.status(500).send(err);
     } else {
-      res.send(rows);
+      res.send(rows); //  objektet res: responsen eller svaret som skicks ifrån vår server till klienten. i detta fall error status 500 eller så skickar den rows.
     }
   });
 });
